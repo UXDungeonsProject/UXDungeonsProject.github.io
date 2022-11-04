ec3b22454deee34733770a5ca71557f01239bf37
Trevor <trevor.lar@gmail.com>
2022-11-04 14:11:28 -0400

    Last try before I switch gears

--- js/NewsScreen.js
+++ js/NewsScreen.js
@@ -1,10 +1,11 @@
 // Javascript code for the news page
-const url = "https://uxdungeonsproject.github.io/SteamNews.php"
+const url = "https://uxdungeonsproject.github.io/php/SteamNews.php"
 function PopulateNews()
 {
     var request = new XMLHttpRequest();
     request.open("GET", url);
     request.setRequestHeader("Content-type", "application/json; charset=UTF-8");
+    request.setRequestHeader("Access-Control-Allow-Origin", "*")
     request.send()
     console.log("Sent request")
     console.log(request.response)

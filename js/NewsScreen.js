99ba52bd66a79a62d25bb2bc54a2e9623e7a30e0
Trevor <trevor.lar@gmail.com>
2022-11-04 17:17:40 -0400

    major features done, styling to go

--- js/NewsScreen.js
+++ js/NewsScreen.js
@@ -2,16 +2,7 @@
 const url = "https://uxdungeonsproject.github.io/php/SteamNews.php"
 function PopulateNews()
 {
-    var request = new XMLHttpRequest();
-    request.open("POST", url);
-    request.setRequestHeader("Content-type", "application/json; charset=UTF-8");
-    request.setRequestHeader("Access-Control-Allow-Origin", "*")
-    console.log("Sent request")
-    request.onload = ()=>{
-        console.log(request.response)
-        console.log(JSON.parse(request.response));
-    }
-    request.send()
+   
 }
 
 

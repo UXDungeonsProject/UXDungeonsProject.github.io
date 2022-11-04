f99f86018912ec12a202c3e568aa09e8340d9665
Trevor <trevor.lar@gmail.com>
2022-11-04 14:37:53 -0400

    Added css to the modes page, and added some more testing to try steam API

--- js/ModesScreen.js
+++ js/ModesScreen.js
@@ -1,5 +1,15 @@
 // Javascript code for the modes page
 
+
+// Steps to add another mode
+// 1.) go to modePage.html, and add your html to the <div> tag that contains the other modes
+    // make sure that your div starts with <div id="modeId" class = "modehidden">
+    // the "modeId" Id will be changed later
+    // everything in the div is up to personal preference, the outside is all that matters
+// 2.) add a new Id to the sectionIds array
+// 3.) change the "id" section from the div earlier to be the new id you just added
+// 4.) done
+
 let sectionIds = new Array(
     "StoryMode",
     "ChallengeDungeons"

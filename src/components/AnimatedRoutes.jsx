ad0ac0de6dc7571edf9b8fd541ae0f8682b8a6f2
TriWing <hunterwnorman@gmail.com>
2022-11-27 17:35:47 -0500

    Navbar mobile styling

--- src/components/AnimatedRoutes.jsx
+++ src/components/AnimatedRoutes.jsx
@@ -2,14 +2,11 @@ import React from 'react';
 import {
   Routes,
   Route,
-  useLocation,
 } from 'react-router-dom';
 
 import { AnimatePresence } from 'framer-motion';
 
 function AnimatedRoutes({ routes }) {
-  const location = useLocation();
-
   return (
     <AnimatePresence>
       <Routes>
--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { motion } from 'framer-motion';
 import Mask from '../components/Mask';
 import '../App.css';
